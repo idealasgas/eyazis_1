23abb89b4bd7e95af1f5eaf86404e781dde1443a
Marie Akhramenko <triangletrue@yahoo.com>
2020-04-22 15:58:56 +0300

    Add some functions

--- lab1.py
+++ lab1.py
@@ -12,8 +12,8 @@ from tkinter import *
 mystem = Mystem()
 russian_stopwords = stopwords.words("russian")
 not_words = [',', '.', '!', '?']
+vocabulary = {}
 
-#Preprocess function
 def preprocess_text(text):
     tokens = mystem.lemmatize(text.lower())
     tokens = [token for token in tokens if token not in russian_stopwords\
@@ -25,41 +25,57 @@ def preprocess_text(text):
 def forms_of_words(text):
   forms = defaultdict(lambda: 0)
   tokens = nltk.word_tokenize(text)
+  tokens.sort()
   for token in tokens:
     if not token in not_words:
       forms[token] += 1
+
+  for form in forms:
+    count = forms[form]
+    forms[form] = [count, '']
   return forms
 
 def get_lexems(text):
   tokens = preprocess_text(text)
+  tokens.sort()
   lexems = defaultdict(lambda: 0)
   for token in tokens:
     if not token in not_words:
       lexems[token] += 1
   return lexems
 
-# preprocess_text("Ворон к ворону летит. Ворон ворону кричит: Ворон! Где б нам отобедать?")
-# forms_of_words('Я очень люблю кушать, но сейчас не кушаю а вот кушала бы и кушала бы')
-# lexems('Я очень люблю кушать, но сейчас не кушаю а вот кушала бы и кушала бы')
-
 def process_text():
   input = text.get(1.0, END)
   forms = forms_of_words(input)
   lexems = get_lexems(input)
+  vocabulary['forms'] = forms
+  vocabulary['lexems'] = lexems
   for key, value in forms.items():
-    tree.insert("", "end", text="%s" % key, values=('%s' % value, ''))
+    tree.insert("", "end", text="%s" % key, values=('%s' % value[0], value[1]))
+  for key, value in lexems.items():
+    lexems_tree.insert("", "end", text="%s" % key, values=('%s' % value))
 
 def add_note():
   text_field.pack()
   submit_button.pack()
 
 def submit():
-  item = tree.selection()
   notes = text_field.get(1.0, END)
   text_field.pack_forget()
   submit_button.pack_forget()
+
+  item = tree.selection()
   value = tree.item(item)['values'][0]
+  text = tree.item(item)['text']
+  vocabulary['forms'][text][1] = notes
   tree.item(item, values=(value, notes))
+  print(vocabulary)
+
+def save_vocabulary():
+  print('hello')
+
+def upload_vocabulary():
+  print('rocketship')
 
 root = tk.Tk()
 
@@ -78,8 +94,22 @@ button.pack(side=LEFT)
 tree = ttk.Treeview()
 tree.pack()
 
+lexems_tree = ttk.Treeview()
+lexems_tree.pack()
+
+lexems_tree["columns"]=("one","two")
+
+lexems_tree.heading("#0",text="Lexems",anchor=tk.W)
+lexems_tree.heading("one", text="Count",anchor=tk.W)
+
 add_note_button = Button(frame, text='Add note', command=add_note)
 add_note_button.pack()
+
+save_vocabulary_button = Button(frame, text='Save to file', command=save_vocabulary)
+save_vocabulary_button.pack()
+
+upload_from_file_button = Button(frame, text='Upload from file', command=upload_vocabulary)
+upload_from_file_button.pack()
 
 tree["columns"]=("one","two")
 
